efbd84658d6d7e9da0d8c1292719f1709e03f280
Anthony Tleiji <anthonytleiji@gmail.com>
2021-07-04 11:35:52 +0300

    Implemented Skeleton for Custom Slider visuals

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -11,82 +11,110 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-        juce::Slider::TextEntryBoxPosition::NoTextBox)
-    {
+	void drawRotarySlider(juce::Graphics&,
+		int x, int y, int width, int height,
+		float sliderPosProportional,
+		float rotaryStartAngle,
+		float rotaryEndAngle,
+		juce::Slider&) override {}
+};
 
-    }
+struct RotarySliderWithLabels : juce::Slider
+{
+	RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+		juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+			juce::Slider::TextEntryBoxPosition::NoTextBox),
+		param(&rap),
+		suffix(unitSuffix)
+	{
+		setLookAndFeel(&lnf);
+	}
 
+	~RotarySliderWithLabels()
+	{
+		setLookAndFeel(nullptr);
+	}
+
+	void paint(juce::Graphics& g) override {}
+	juce::Rectangle<int> getSliderBounds() const; 
+	int getTextHeight() const { return 14; }
+	juce::String getDisplayString() const;
+
+private:
+	LookAndFeel lnf;
+
+	juce::RangedAudioParameter* param;
+	juce::String suffix;
 };
 
 struct ResponseCurveComponent : juce::Component,
-    juce::AudioProcessorParameter::Listener,
-    juce::Timer
+	juce::AudioProcessorParameter::Listener,
+	juce::Timer
 {
-    ResponseCurveComponent(SimpleEQAudioProcessor&);
-    ~ResponseCurveComponent();
+	ResponseCurveComponent(SimpleEQAudioProcessor&);
+	~ResponseCurveComponent();
 
-    void parameterValueChanged(int parameterIndex, float newValue) override;
+	void parameterValueChanged(int parameterIndex, float newValue) override;
 
-    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
+	void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
 
-    void timerCallback() override;
+	void timerCallback() override;
 
-    void paint(juce::Graphics& g) override;
-    //void resized() override;
+	void paint(juce::Graphics& g) override;
+	//void resized() override;
 
 private:
 
-    SimpleEQAudioProcessor& audioProcessor;
+	SimpleEQAudioProcessor& audioProcessor;
 
-    juce::Atomic<bool> parametersChanged{ false };
+	juce::Atomic<bool> parametersChanged{ false };
 
-    MonoChain monoChain;
+	MonoChain monoChain;
 };
 
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
-    ~SimpleEQAudioProcessorEditor() override;
+	SimpleEQAudioProcessorEditor(SimpleEQAudioProcessor&);
+	~SimpleEQAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    SimpleEQAudioProcessor& audioProcessor;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	SimpleEQAudioProcessor& audioProcessor;
 
-    CustomRotarySlider peakFreqSlider,
-        peakGainSlider,
-        peakQualitySlider,
-        lowCutFreqSlider,
-        highCutFreqSlider,
-        lowCutSlopeSlider,
-        highCutSlopeSlider;
+	RotarySliderWithLabels peakFreqSlider,
+		peakGainSlider,
+		peakQualitySlider,
+		lowCutFreqSlider,
+		highCutFreqSlider,
+		lowCutSlopeSlider,
+		highCutSlopeSlider;
 
-    ResponseCurveComponent responseCurveComponent;
+	ResponseCurveComponent responseCurveComponent;
 
-    using APVTS = juce::AudioProcessorValueTreeState;
-    using Attachment = APVTS::SliderAttachment;
+	using APVTS = juce::AudioProcessorValueTreeState;
+	using Attachment = APVTS::SliderAttachment;
 
-    Attachment peakFreqSliderAttachment,
-        peakGainSliderAttachment,
-        peakQualitySliderAttachment,
-        lowCutFreqSliderAttachment,
-        highCutFreqSliderAttachment,
-        lowCutSlopeSliderAttachment,
-        highCutSlopeSliderAttachment;
+	Attachment peakFreqSliderAttachment,
+		peakGainSliderAttachment,
+		peakQualitySliderAttachment,
+		lowCutFreqSliderAttachment,
+		highCutFreqSliderAttachment,
+		lowCutSlopeSliderAttachment,
+		highCutSlopeSliderAttachment;
 
 
-    std::vector<juce::Component*> getComps();
+	std::vector<juce::Component*> getComps();
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor)
 };
